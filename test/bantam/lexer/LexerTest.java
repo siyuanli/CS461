c9ec6e652383784cadacca19a99a6ae90930de12
Siyuan Li <siyuanli@colby.edu>
2017-02-10 18:51:28 -0500

    Added more cases

--- test/bantam/lexer/LexerTest.java
+++ test/bantam/lexer/LexerTest.java
@@ -49,6 +49,157 @@ public class LexerTest
     }
 
     @Test
+    public void newToken() throws Exception {
+        checkToken(" new ", "NEW");
+    }
+
+    @Test
+    public void instanceofToken() throws Exception {
+        checkToken(" instanceof ", "INSTANCEOF");
+    }
+
+    @Test
+    public void forToken() throws Exception {
+        checkToken(" for ", "FOR");
+    }
+
+    @Test
+    public void whileToken() throws Exception {
+        checkToken(" while ", "WHILE");
+    }
+
+    @Test
+    public void ifToken() throws Exception {
+        checkToken(" if ", "IF");
+    }
+
+    @Test
+    public void elseToken() throws Exception {
+        checkToken(" else ", "ELSE");
+    }
+
+    @Test
+    public void returnToken() throws Exception {
+        checkToken(" return ", "RETURN");
+    }
+
+    @Test
+    public void breakToken() throws Exception {
+        checkToken(" break ", "BREAK");
+    }
+
+    @Test
+    public void semiToken() throws Exception {
+        checkToken(" ; ", "SEMI");
+    }
+
+    @Test
+    public void commmaToken() throws Exception {
+        checkToken(" , ", "COMMA");
+    }
+
+    @Test
+    public void lparenToken() throws Exception {
+        checkToken(" ( ", "LPAREN");
+    }
+
+    @Test
+    public void rparenToken() throws Exception {
+        checkToken(" ) ", "RPAREN");
+    }
+
+    @Test
+    public void lsqbraceToken() throws Exception {
+        checkToken(" [ ", "LSQBRACE");
+    }
+
+    @Test
+    public void rsqbraceToken() throws Exception {
+        checkToken(" ] ", "RSQBRACE");
+    }
+
+    @Test
+    public void lbraceToken() throws Exception {
+        checkToken(" { ", "LBRACE");
+    }
+
+    @Test
+    public void rbraceToken() throws Exception {
+        checkToken(" } ", "RBRACE");
+    }
+
+    @Test
+    public void minusToken() throws Exception {
+        checkToken(" - ", "MINUS");
+    }
+
+    @Test
+    public void plusToken() throws Exception {
+        checkToken(" + ", "PLUS");
+    }
+
+    @Test
+    public void divideToken() throws Exception {
+        checkToken(" / ", "DIVIDE");
+    }
+
+    @Test
+    public void timesToken() throws Exception {
+        checkToken(" * ", "TIMES");
+    }
+
+    @Test
+    public void modulusToken() throws Exception {
+        checkToken(" % ", "MODULUS");
+    }
+
+    @Test
+    public void decrToken() throws Exception {
+        checkToken(" -- ", "DECR");
+    }
+
+    @Test
+    public void incrToken() throws Exception {
+        checkToken(" ++ ", "INCR");
+    }
+
+    @Test
+    public void ltToken() throws Exception {
+        checkToken(" < ", "LT");
+    }
+
+    @Test
+    public void gtToken() throws Exception {
+        checkToken(" > ", "GT");
+    }
+
+    @Test
+    public void leqToken() throws Exception {
+        checkToken(" <= ", "LEG");
+    }
+
+    @Test
+    public void geqToken() throws Exception {
+        checkToken(" >= ", "GEG");
+    }
+
+    @Test
+    public void assignToken() throws Exception {
+        checkToken(" = ", "ASSIGN");
+    }
+
+    @Test
+    public void eqToken() throws Exception {
+        checkToken(" == ", "EG");
+    }
+
+    @Test
+    public void neToken() throws Exception {
+        checkToken(" != ", "NE");
+    }
+
+
+    @Test
     public void basicStringToken() throws Exception {
         checkToken(" \"hi\" ", "STRING_CONST");
     }
