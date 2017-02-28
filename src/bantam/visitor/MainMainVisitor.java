b0080f4f4c4569afd9b24cdd0cc3901c1685de5d
Siyuan Li <siyuanli@colby.edu>
2017-02-27 19:49:57 -0500

    Add comments

--- src/bantam/visitor/MainMainVisitor.java
+++ src/bantam/visitor/MainMainVisitor.java
@@ -6,15 +6,28 @@
  */
 
 package bantam.visitor;
+
 import bantam.ast.*;
 
-
+/**
+ * Vistor class for checking a given program has a Main class
+ * with a main method and a void return type
+ */
 public class MainMainVisitor extends Visitor {
-
+    /**
+     * Check if root node has a Main class with a main method and a void return type
+     * @param root the root node of the program
+     * @return a boolean
+     */
     public boolean hasMain(ASTNode root){
         return (boolean)root.accept(this);
     }
 
+    /**
+     * Check if any nodes in the list has a Main class with a main method
+     * @param nodes a list of nodes
+     * @return a boolean indicating if there is a Main class and a main method
+     */
     private boolean listNodeHasMain(ListNode nodes) {
         for (ASTNode node: nodes){
             boolean hasMain = (boolean)node.accept(this);
@@ -25,16 +38,31 @@ public class MainMainVisitor extends Visitor {
         return false;
     }
 
+    /**
+     * Check if the program node has a Main class and a main method
+     * @param node the program node
+     * @return a boolean indicating if there is a Main class and a main method
+     */
     @Override
     public Object visit(Program node) {
         return node.getClassList().accept(this);
     }
 
+    /**
+     * Check if any class nodes has a Main class and a main method
+     * @param nodes a list of class nodes
+     * @return a boolean indicating if there is a Main class and a main method
+     */
     @Override
     public Object visit(ClassList nodes){
         return this.listNodeHasMain(nodes);
     }
 
+    /**
+     * Check if a class node has a Main class and a main method
+     * @param node the class node
+     * @return a boolean indicating if there is a Main class and a main method
+     */
     @Override
     public Object visit(Class_ node) {
         boolean nameMain = node.getName().toLowerCase().equals("main");
@@ -44,16 +72,31 @@ public class MainMainVisitor extends Visitor {
         return false;
     }
 
+    /**
+     * Check if any member nodes has a Main class and a main method
+     * @param nodes a list of memberlist nodes
+     * @return a boolean indicating if there is a Main class and a main method
+     */
     @Override
     public Object visit(MemberList nodes){
        return this.listNodeHasMain(nodes);
     }
 
+    /**
+     * Return false for field nodes to prune the AST
+     * @param node the field node
+     * @return return false
+     */
     @Override
     public Object visit(Field node) {
         return false;
     }
 
+    /**
+     * Check if a method node has a Main class and a main method
+     * @param node the method node
+     * @return a boolean indicating if there is a Main class and a main method
+     */
     @Override
     public Object visit(Method node) {
         boolean nameMain = node.getName().toLowerCase().equals("main");
